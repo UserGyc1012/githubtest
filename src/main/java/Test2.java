4da7a5f2f2f3be6144fee958faa8f6686db4cb4d
offcn-java <myqq@qq.com>
2020-11-05 21:26:47 +0800

    222

---
+++ src/main/java/Test2.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Test2
+ * @Deacription TODO
+ * @Author 葛言超
+ * @Date 2020/11/5 21:26
+ * version:1.0 * 注意：本内容仅限于公司内部传阅，禁止外泄以及用于其他的商业目
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("test3");
+    }
+}
