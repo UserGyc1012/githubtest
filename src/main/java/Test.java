6bfa6a0a97ba52df1b6cfdf27dbdac86d1ea0376
offcn-java <myqq@qq.com>
2020-11-05 21:24:44 +0800

    111

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Test
+ * @Deacription TODO
+ * @Author 葛言超
+ * @Date 2020/11/5 21:21
+ * version:1.0 * 注意：本内容仅限于公司内部传阅，禁止外泄以及用于其他的商业目
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("这是一个测试");
+    }
+}
