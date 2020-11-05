5a408f66fb8d1c07b29dd7bf86054cc93672ff04
offcn-java <myqq@qq.com>
2020-11-05 21:14:44 +0800

    Initial commit

---
+++ src/main/java/HelloWord.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName HelloWord
+ * @Deacription TODO
+ * @Author 葛言超
+ * @Date 2020/11/5 21:11
+ * version:1.0 * 注意：本内容仅限于公司内部传阅，禁止外泄以及用于其他的商业目
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("HelloWord");
+    }
+}
